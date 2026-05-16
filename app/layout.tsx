4fd3aa8a8708a329fa4e62413508a41b2e9ad8fb
mohamed ali <mohamedandali0123@gmail.com>
2026-05-16 09:44:22 +0300

    change fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const manrope = Manrope({
 export const metadata: Metadata = {
   title: "Marble Plus Construction | Exclusive Stone Collection",
   description: "Exclusive Stone Collection and Construction Projects",
-  icons: { icon: "/my-icon.webp" }
+  icons: { icon: "/fav.webp" }
 };
 
 export default function RootLayout({ children }: Readonly<{
